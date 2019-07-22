c4e59716b036d09de6c3bd94c44b9f09b3a10fdc
Andrew Vitale <vitale232@gmail.com>
2019-07-22 10:31:18 -0400

    Move scheduled job to its own dir

--- scheduled/scheduled_job.bat
+++ scheduled/scheduled_job.bat
@@ -1,3 +1,3 @@
 call D:\Program_Files\miniconda3\condabin\activate.bat test
-python D:\OpenStreetMap\osmdiff_viewer\scripts\scheduled_job.py
+python D:\OpenStreetMap\osmdiff_viewer\scheduled\scheduled_job.py
 call conda deactivate
